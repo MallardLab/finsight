a07622429f3d36e309b8cace976a9d150415a5e3
Marcos Figueiredo <marcos.figueiredo.masf@gmail.com>
2025-03-03 16:05:48 -0300

    build: setup `tailwind v4`

--- packages/ui/src/components/hello-world.tsx
+++ packages/ui/src/components/hello-world.tsx
@@ -1,9 +1,9 @@
 const HelloWorld = ({
-  className = "bg-red-700",
+  className = "bg-green-700",
 }: {
   className?: string;
 }) => {
-  return <h1 className={className}>Hello World</h1>;
+  return <h1 className={className}>Helloooo World</h1>;
 };
 
 export { HelloWorld };
